fb2cfa377429544760adeebbe852b74af08ab1d1
TKG25 <theking.games25@gmail.com>
2022-02-09 12:04:19 +0100

    Update Streams.java

--- src/main/java/nl/han/ica/oose/dea/Streams.java
+++ src/main/java/nl/han/ica/oose/dea/Streams.java
@@ -4,39 +4,50 @@ package nl.han.ica.oose.dea;
 import nl.han.ica.oose.dea.helpers.Product;
 import nl.han.ica.oose.dea.helpers.ProductCategory;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class Streams {
 
     List<String> filterStringsShorterThanThreeCharacters(List<String> input) {
-        // TODO: implement
-        return input;
+        return input.stream()
+                .filter(s -> s.length()<3)
+                .collect(Collectors.toList());
+
     }
 
     List<String> filterStringsThatContainOnlyNumerals(List<String> input) {
-        // TODO: implement
-        return input;
+        return input.stream()
+                .filter(s -> s.chars().allMatch(c -> Character.isDigit(c)))
+                .collect(Collectors.toList());
+
     }
 
     String findShortestString(List<String> input) {
-        // TODO: implement
-        return "";
+        return input.stream()
+                .min(Comparator.comparingInt(s -> s.length()))
+                .orElse(null);
     }
 
     String createAFullSentenceFromTheList(List<String> input) {
-        // TODO: implement
-        return "";
+        return input.stream()
+                .collect(Collectors.joining(" "));
     }
 
     int calculateTotalCostOfAllProducts(List<Product> products) {
-        // TODO: implement
-        return 0;
+        return products.stream()
+                .mapToInt(p -> p.getPrice())
+                .sum();
     }
 
     int calculateTotalCostOfAllGadgets(List<Product> products) {
-        // TODO: implement
-        return 0;
+        return products.stream()
+                .filter(product -> product.getCategory().equals(ProductCategory.GADGETS))
+                .mapToInt(p -> p.getPrice())
+                .sum();
     }
 }
